70e7a697db6b14c7cb263ab30bc6dbf94142ab77
Damon Self <damongself@gmail.com>
2020-03-14 22:13:15 -0400

    Removes byebug from json_web_token.rb
    Accidentally left byebug in json_web_token.rb and pushed to heroku. It
    is now removed.

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,7 +2,6 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      byebug
       JWT.encode(payload, HEROKU_SECRET_KEY_BASE)
     end
 
